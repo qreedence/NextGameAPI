a249896a2ed1e2784da936827f0a9f433415a1e2
qreedence <eden.yusof.ioannidis@gmail.com>
2025-01-29 14:41:35 +0100

    Implement Identity

--- Program.cs
+++ Program.cs
@@ -1,5 +1,10 @@
 using DotNetEnv;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using NextGameAPI.Data;
+using NextGameAPI.Data.Models;
 using Scalar.AspNetCore;
+using System;
 
 namespace NextGameAPI
 {
@@ -12,16 +17,31 @@ namespace NextGameAPI
             // Add services to the container.
 
             builder.Services.AddControllers();
-            // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
             builder.Services.AddOpenApi();
 
+            //DotNetEnv
             Env.Load();
 
+            //DbContext
+            builder.Services.AddDbContext<ApplicationDbContext>(options =>
+                            options.UseSqlServer(Environment.GetEnvironmentVariable("connection-string")));
+
+            //Identity
+            builder.Services.AddIdentityCore<User>()
+                .AddRoles<IdentityRole>()
+                .AddEntityFrameworkStores<ApplicationDbContext>()
+                .AddDefaultTokenProviders();
+
+            builder.Services.AddAuthentication();
+
+            builder.Services.AddAuthorization();
+
+            //CORS
             builder.Services.AddCors(options =>
             {
                 options.AddDefaultPolicy(builder =>
                 {
-                    builder.WithOrigins(Environment.GetEnvironmentVariable("cors-client-https-url"), Environment.GetEnvironmentVariable("cors-client-http-url"))
+                    builder.WithOrigins(Environment.GetEnvironmentVariable("cors-client-https-url")!, Environment.GetEnvironmentVariable("cors-client-http-url")!)
                     .AllowAnyOrigin()
                     .AllowAnyHeader();
                 });
@@ -41,7 +61,7 @@ namespace NextGameAPI
             app.UseHttpsRedirection();
 
             app.UseAuthorization();
-
+            app.UseAuthentication();
 
             app.MapControllers();
 
